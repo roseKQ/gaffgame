8914caadf936499705b56f2807e1b8c1f91af9be
Ann <a.fitzy@hotmail.com>
2016-11-10 14:06:14 +0000

    changed dropdown code in html to make JS easier

---
+++ datatypes.js
@@ -0,0 +1,20 @@
+
+function datatypes(){
+    var myList=document.getElementById("myList");
+    document.getElementById("message").innerHTML = myList;
+
+    /*var isCorrect = false;
+    var correctAnswer = "int";
+
+    console.log(isCorrect);
+    console.log(correctAnswer);
+    console.log(myList);*/
+
+   /* if(myList != correctAnswer){
+      isCorrect=true;
+      console.log(isCorrect);
+    }else{
+      console.log(isCorrect);
+    }*/
+    
+    }
