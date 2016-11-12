ad0ed7c7ca3b899026bcbf149568a26d1fa42afa
Ann <a.fitzy@hotmail.com>
2016-11-12 15:52:34 +0000

    Most functionality working now.

--- datatypes.js
+++ datatypes.js
@@ -1,32 +1,56 @@
+var score=0;
 
-function datatypes(menu){
+
+function datatypes(menu, correct){
+
+
+
     var selection=menu.options[menu.selectedIndex].text;
+    var correctAnswer = menu.options[menu.Index = correct].text;
     var isCorrect = false;
-    var correctAnswer = "Int";
-    var score = 0;
-    var noOfQuestions = 5;
+   // var correctAnswer = document.getElementById("menu").selectedIndex.text = correct;
+   
+   // var score = 0;
+    //var noOfQuestions = 5;
 
-    console.log(selection);
+   console.log(selection);
     console.log(isCorrect);
     console.log(correctAnswer);
-    
-   
+    //console.log(selectedValue);
+    //console.log(correctValue);
 
+   
+   
+  
     if(selection == correctAnswer){
       isCorrect=true;
       console.log(isCorrect);
-      score ++;
+      score += 1;
       console.log(score);
-      moveProgressBar();
+      move();
     }else{
+      score=score;
       console.log(isCorrect);
       console.log(score);
-      moveProgressBar();
+      
+      move();
     }
     
     }
 
-    function moveProgressBar() {
-    var progress = noOfQuestions;
-    
-  }
+
+    function move() {
+    var elem = document.getElementById("myBar"); 
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++; 
+            elem.style.width = width + '%'; 
+        }
+    }
+}
+
+
